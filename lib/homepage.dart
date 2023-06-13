7a932acea0789620ea6b2092e28c4002b21e495e
Khem Raksa Peou <kraksaprofessional@gmail.com>
2023-06-13 08:00:11 +0700

    Pick File

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:file_picker/file_picker.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -8,6 +10,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late String _filepath;
+
+  void _openFilePicker() async {
+    try{
+
+      FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.image
+      );
+
+      if (result!=null){
+        setState(() {
+          _filepath = result.files.single.path!;
+        });
+      }
+
+    } on PlatformException catch(e) {
+      print("Error while picking the file: $e");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
